3fc6b3c3651234b285a97a02e845537d5f789f77
kakakaya <kakakaya+git@gmail.com>
2014-10-21 11:29:00 +0900

    small indent fix

--- script/main.js
+++ script/main.js
@@ -1,24 +1,27 @@
-$(function() {
+$(document).ready(function() {
     $('.later').hide();
     $('a').hover(
 	function() {
-	    $(this).append($("<span> </span>"));
+	    $(this).append($("<span></span>"));
 	},
 	function() {
 	    $(this).find("span:last").remove();
 	}
     );
     $('h2').hover(
+	// h2のonMouseで
 	function() {
+	    // 動いてなければ
 	    if ($('*').not(':animated')) {
 		$(this).parent(".category").children('.later')
 		    .animate({opacity:"toggle", "height":"toggle"}, "fast");
 	    }
 	},
 	function() {
+	    // 動いてれば
 	    if ($('*').not(':animated')) {
-	    $(this).parent(".category").children('.later')
-		.animate({opacity:"toggle", "height":"toggle"}, "fast");
+		$(this).parent(".category").children('.later')
+		    .animate({opacity:"toggle", "height":"toggle"}, "fast");
 	    }
 	}
     );
