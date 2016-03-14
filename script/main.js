b0bee07511eb077a9134d163cecd50518b57e1bb
kakakaya <kakakaya+git@gmail.com>
2016-03-14 20:18:36 +0900

    loding.css追加,モバイル対応

--- script/main.js
+++ script/main.js
@@ -26,37 +26,44 @@ $(function() {
       $(".ui-widget-overlay").on("click", function() {
         dialog.dialog("close");
       });
-    },
-    close: function() {
-      $("#dialog_content").html("<h3>Now loading!</h3><p>読み込み中です。</p>");
     }
   });
 
+  var loading_html = '<h3>Now loading!</h3><p>読み込み中です。</p><div class="spinner"><div class="cube1"></div><div class="cube2"></div></div>';
+
   $(".interest").click(function() {
     var title = $.trim($(this).text());
-    var url = $(this).attr("data-href");
+    // GAS側の更新に合わせてここを変える必要あり
+    var url = "https://script.google.com/macros/s/AKfycbxQTXf_cRFiqPFVEHJVKEEBUrns9sG82eSMpdFve9U676wY6B4f/exec";
+    var alt_url = $(this).attr("alt-href");
+    console.log(url);
+    console.log(alt_url);
     dialog.dialog("option", "title", title);
-    console.log(url == "");
-    if (url === "") {
-      $("#dialog_content").html("<h3>Unknown Type!</h3><p>"+title+":この項目のデータをまだ作っていません。</p>");
-    } else {
-      $("#dialog_content").html("<h3>Now loading!</h3><p>読み込み中です。</p>");
-      $.ajax({
-        dataType: "json",
-        data: "key=KEY",
-        timeout: 5000,
-        url: url,
-        success: function(data, status) {
-          console.log(data);
-          console.log(status);
-          if (status == "success") {
-            $("#dialog_content").html(makeDialogContent(title, data));
-          } else {
-            $("#dialog_content").html("<p>取得に失敗しました。</p>");
-          }
+
+    // 読み込み中……
+    $("#dialog_content").html(loading_html);
+
+    // 非同期でデータ取得
+    $.ajax({
+      dataType: "json",
+      data: "key=KEY&interest="+title,
+      timeout: 5000,
+      url: url,
+      success: function(data, status) {
+        if (status == "success") {
+          $("#dialog_content").html(data.html+"<p>もっと詳しく見たい場合は<a href=\""+alt_url+"\" target=\"_blank\">こちらを参照してみてください。</a></p>");
+        } else {
+          $("#dialog_content").html("<p>取得に失敗しました。</p><p>かわりに<a href=\""+alt_url+"\" target=\"_blank\">こちらを参照してみてください。</a></p>");
         }
-      });
-    }
+      },
+      error: function(data, status) {
+        if (status == "timeout") {
+          $("#dialog_content").html("<h3>Timeout</h3><p>読み込みに時間がかかっているようです。</p><p>かわりに<a href=\""+alt_url+"\" target=\"_blank\">こちらを参照してみてください。</a></p>");
+        }
+      }
+    });
+
+    // データを待ちつつダイアログを表示する
     dialog.dialog("open");
   });
 
